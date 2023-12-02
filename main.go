49267669a61d09cd4b2e5a4d7fd4e9de15293591
willbraun <williamhbraun1@gmail.com>
2023-12-02 11:04:14 -0500

    uncomment

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
-	// "os"
+	"os"
 
 	"github.com/pocketbase/pocketbase"
-	// "github.com/pocketbase/pocketbase/apis"
-	// "github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
@@ -15,11 +15,11 @@ func main() {
 
 	fmt.Println("hi")
 
-	// serves static files from the provided public dir (if exists)
-	// app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-	//     e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
-	//     return nil
-	// })
+	serves static files from the provided public dir (if exists)
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+	     e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+	     return nil
+	 })
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
