c775107722e5f465380a7a04c30c0f6bc0c51855
willbraun <williamhbraun1@gmail.com>
2024-05-03 13:49:45 -0600

    added unit test for point updates

--- main_test.go
+++ main_test.go
@@ -55,10 +55,11 @@ func generateRecordToken(collectionNameOrId string, username string) (string, er
 	return tokens.NewRecordAuthToken(app, record)
 }
 
+const drawId = "2l1hqqi8puodmjq"
+
 func TestPredictionCloseUpdate(t *testing.T) {
 	assert := assert.New(t)
 
-	const drawId = "2l1hqqi8puodmjq"
 	const r16Slot15Id = "4pbsipqoncnd14h"
 	const r16Slot16Id = "7wg2gmjqutu1bky"
 	const qfSlot1Id = "y6aj4a1vxfibcfv"
@@ -67,6 +68,10 @@ func TestPredictionCloseUpdate(t *testing.T) {
 	recordToken, err := generateRecordToken("user", "script_user")
 	if err != nil {
 		t.Fatal(err)
+	}
+
+	requestHeaders := map[string]string{
+		"Authorization": recordToken,
 	}
 
 	scenarios := []tests.ApiScenario{
@@ -81,9 +86,7 @@ func TestPredictionCloseUpdate(t *testing.T) {
 				Name:     "Mertens",
 				Seed:     "(13)",
 			}),
-			RequestHeaders: map[string]string{
-				"Authorization": recordToken,
-			},
+			RequestHeaders: requestHeaders,
 			ExpectedStatus:  200,
 			ExpectedContent: []string{"\"collectionName\":\"draw_slot\""},
 			ExpectedEvents: map[string]int{
@@ -142,9 +145,7 @@ func TestPredictionCloseUpdate(t *testing.T) {
 				Name:     "Rybakina",
 				Seed:     "(2)",
 			}),
-			RequestHeaders: map[string]string{
-				"Authorization": recordToken,
-			},
+			RequestHeaders: requestHeaders,
 			ExpectedStatus:  200,
 			ExpectedContent: []string{"\"collectionName\":\"draw_slot\""},
 			ExpectedEvents: map[string]int{
@@ -213,9 +214,7 @@ func TestPredictionCloseUpdate(t *testing.T) {
 				Name:     "Sabalenka",
 				Seed:     "(1)",
 			}),
-			RequestHeaders: map[string]string{
-				"Authorization": recordToken,
-			},
+			RequestHeaders: requestHeaders,
 			ExpectedStatus:  200,
 			ExpectedContent: []string{"\"collectionName\":\"draw_slot\""},
 			ExpectedEvents: map[string]int{
@@ -292,6 +291,169 @@ func TestPredictionCloseUpdate(t *testing.T) {
 				assert.Equal(len(r16FilledSlots), 16)
 				assert.NotEmpty(draw.GetDateTime("prediction_close"))
 			},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		scenario.Test(t)
+	}
+}
+
+func TestPointUpdate(t *testing.T) {
+	setupTestApp := func(t *testing.T) *tests.TestApp {
+		testApp, err := tests.NewTestApp(testDataDir)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		bindAppHooks(testApp)
+
+		return testApp
+	}
+
+	setupBeforeTest := func(assert *assert.Assertions, winningPrediction string, losingPrediction string) func(t *testing.T, app *tests.TestApp, e *echo.Echo) {
+		return func(t *testing.T, app *tests.TestApp, e *echo.Echo) {
+			win, err := app.Dao().FindRecordById("prediction", winningPrediction)
+			if err != nil {
+				log.Println("Error accessing winning prediction,", err)
+			}
+
+			lose, err := app.Dao().FindRecordById("prediction", losingPrediction)
+			if err != nil {
+				log.Println("Error accessing winning prediction,", err)
+			}
+
+			assert.Equal(win.GetInt("points"), 0)
+			assert.Equal(lose.GetInt("points"), 0)
+		}
+	}
+
+	setupAfterTest := func(assert *assert.Assertions, winningPrediction string, losingPrediction string, expectedPoints int) func(t *testing.T, app *tests.TestApp, res *http.Response) {
+		return func(t *testing.T, app *tests.TestApp, res *http.Response) {
+			win, err := app.Dao().FindRecordById("prediction", winningPrediction)
+			if err != nil {
+				log.Println("Error accessing winning prediction,", err)
+			}
+
+			lose, err := app.Dao().FindRecordById("prediction", losingPrediction)
+			if err != nil {
+				log.Println("Error accessing winning prediction,", err)
+			}
+
+			assert.Equal(win.GetInt("points"), expectedPoints)
+			assert.Equal(lose.GetInt("points"), 0)
+		}
+	}
+
+	assert := assert.New(t)
+
+	const quarterfinalSlotId = "y6aj4a1vxfibcfv"
+	const semifinalSlotId = "43objik3hkdl34k"
+	const finalSlotId = "alsia0d829o6qox"
+	const winnerSlotId = "t1arj8fwjbcfltm"
+
+	const quarterfinalWinningPrediction = "ahsplh4qx7zkwkt"
+	const semifinalWinningPrediction = "w0gko3g832lihmm"
+	const finalWinningPrediction = "r9nhh355zrokdhi"
+	const winnerWinningPrediction = "3x4hc8ikjugec9a"
+
+	const quarterfinalLosingPrediction = "tagsi9hyse5i8rz"
+	const semifinalLosingPrediction = "yfzbezk7pvkofn6"
+	const finalLosingPrediction = "ohobwbtobq156mu"
+	const winnerLosingPrediction = "edbd19e25ersljg"
+
+	expectedEvents := map[string]int{
+		"OnModelAfterUpdate":          2,
+		"OnModelBeforeUpdate":         2,
+		"OnRecordAfterUpdateRequest":  1,
+		"OnRecordBeforeUpdateRequest": 1,
+	}
+
+	recordToken, err := generateRecordToken("user", "script_user")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	requestHeaders := map[string]string{
+		"Authorization": recordToken,
+	}
+
+	scenarios := []tests.ApiScenario{
+		{
+			Name:   "Quarterfinal prediction result",
+			Method: http.MethodPatch,
+			Url:    fmt.Sprintf("/api/collections/draw_slot/records/%s", quarterfinalSlotId),
+			Body: getIoReaderBody(CreateUpdateSlotReq{
+				DrawID:   drawId,
+				Round:    4,
+				Position: 1,
+				Name:     "Sabalenka",
+				Seed:     "(1)",
+			}),
+			RequestHeaders: requestHeaders,
+			ExpectedStatus:  200,
+			ExpectedContent: []string{"\"collectionName\":\"draw_slot\""},
+			ExpectedEvents:  expectedEvents,
+			TestAppFactory:  setupTestApp,
+			BeforeTestFunc:  setupBeforeTest(assert, quarterfinalWinningPrediction, quarterfinalLosingPrediction),
+			AfterTestFunc:   setupAfterTest(assert, quarterfinalWinningPrediction, quarterfinalLosingPrediction, 1),
+		},
+		{
+			Name:   "Semifinal prediction result",
+			Method: http.MethodPatch,
+			Url:    fmt.Sprintf("/api/collections/draw_slot/records/%s", semifinalSlotId),
+			Body: getIoReaderBody(CreateUpdateSlotReq{
+				DrawID:   drawId,
+				Round:    5,
+				Position: 1,
+				Name:     "Sabalenka",
+				Seed:     "(1)",
+			}),
+			RequestHeaders: requestHeaders,
+			ExpectedStatus:  200,
+			ExpectedContent: []string{"\"collectionName\":\"draw_slot\""},
+			ExpectedEvents:  expectedEvents,
+			TestAppFactory:  setupTestApp,
+			BeforeTestFunc:  setupBeforeTest(assert, semifinalWinningPrediction, semifinalLosingPrediction),
+			AfterTestFunc:   setupAfterTest(assert, semifinalWinningPrediction, semifinalLosingPrediction, 2),
+		},
+		{
+			Name:   "Final prediction result",
+			Method: http.MethodPatch,
+			Url:    fmt.Sprintf("/api/collections/draw_slot/records/%s", finalSlotId),
+			Body: getIoReaderBody(CreateUpdateSlotReq{
+				DrawID:   drawId,
+				Round:    6,
+				Position: 1,
+				Name:     "Sabalenka",
+				Seed:     "(1)",
+			}),
+			RequestHeaders: requestHeaders,
+			ExpectedStatus:  200,
+			ExpectedContent: []string{"\"collectionName\":\"draw_slot\""},
+			ExpectedEvents:  expectedEvents,
+			TestAppFactory:  setupTestApp,
+			BeforeTestFunc:  setupBeforeTest(assert, finalWinningPrediction, finalLosingPrediction),
+			AfterTestFunc:   setupAfterTest(assert, finalWinningPrediction, finalLosingPrediction, 4),
+		},
+		{
+			Name:   "Winner prediction result",
+			Method: http.MethodPatch,
+			Url:    fmt.Sprintf("/api/collections/draw_slot/records/%s", winnerSlotId),
+			Body: getIoReaderBody(CreateUpdateSlotReq{
+				DrawID:   drawId,
+				Round:    7,
+				Position: 1,
+				Name:     "Rybakina",
+				Seed:     "(2)",
+			}),
+			RequestHeaders: requestHeaders,
+			ExpectedStatus:  200,
+			ExpectedContent: []string{"\"collectionName\":\"draw_slot\""},
+			ExpectedEvents:  expectedEvents,
+			TestAppFactory:  setupTestApp,
+			BeforeTestFunc:  setupBeforeTest(assert, winnerWinningPrediction, winnerLosingPrediction),
+			AfterTestFunc:   setupAfterTest(assert, winnerWinningPrediction, winnerLosingPrediction, 8),
 		},
 	}
 
