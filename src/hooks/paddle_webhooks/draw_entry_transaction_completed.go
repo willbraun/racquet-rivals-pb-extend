0a48f452bcecc6488c6e52e2e36bab2f995f5388
willbraun <williamhbraun1@gmail.com>
2025-03-22 13:34:29 -0400

    starting paddle webhook

---
+++ src/hooks/paddle_webhooks/draw_entry_transaction_completed.go
@@ -0,0 +1,47 @@
+package hooks
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/pocketbase/pocketbase/core"
+)
+
+// Check if a given user has access to participate in a given draw
+func RegisterDrawEntryTransactionCompletedHook(app core.App) {
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		se.Router.POST("/webhook/draw-entry-transaction-completed", func(e *core.RequestEvent) error {
+			bodyBytes, err := io.ReadAll(e.Request.Body)
+			if err != nil {
+				return e.JSON(http.StatusBadRequest, map[string]interface{}{
+					"error": "Failed to read request body",
+				})
+			}
+			defer e.Request.Body.Close()
+
+			var requestBody PaddleTransactionCompleted
+			if err := json.Unmarshal(bodyBytes, &requestBody); err != nil {
+				return e.JSON(http.StatusBadRequest, map[string]interface{}{
+					"error":   "Invalid JSON format",
+					"details": err.Error(),
+				})
+			}
+
+			// Get the user who made the purchase, by user_id in custom_data
+			// If no user or user not found, throw error (and send me email?)
+
+			// Parse the request body to get the product purchased (Men, Women, or Both)
+			// if product not recognized, throw error
+
+			// Get the active draw(s) of that product type
+
+			// Create a new draw entry for the user and the active draw(s)
+			// if this errors, throw error
+
+			// Return a success response
+		})
+
+		return se.Next()
+	})
+}
---
+++ src/hooks/paddle_webhooks/types.go
@@ -0,0 +1,179 @@
+package hooks
+
+import "time"
+
+type PaddleTransactionCompleted struct {
+	EventID        string                   `json:"event_id"`
+	EventType      string                   `json:"event_type"`
+	OccurredAt     time.Time                `json:"occurred_at"`
+	NotificationID string                   `json:"notification_id"`
+	Data           TransactionCompletedData `json:"data"`
+}
+
+type TransactionCompletedData struct {
+	ID             string             `json:"id"`
+	Items          []TransactionItem  `json:"items"`
+	Origin         string             `json:"origin"`
+	Status         string             `json:"status"`
+	Details        TransactionDetails `json:"details"`
+	Checkout       Checkout           `json:"checkout"`
+	Payments       []Payment          `json:"payments"`
+	BilledAt       time.Time          `json:"billed_at"`
+	AddressID      string             `json:"address_id"`
+	CreatedAt      time.Time          `json:"created_at"`
+	InvoiceID      string             `json:"invoice_id"`
+	UpdatedAt      time.Time          `json:"updated_at"`
+	RevisedAt      *time.Time         `json:"revised_at"`
+	BusinessID     *string            `json:"business_id"`
+	CustomData     interface{}        `json:"custom_data"`
+	CustomerID     string             `json:"customer_id"`
+	DiscountID     *string            `json:"discount_id"`
+	CurrencyCode   string             `json:"currency_code"`
+	BillingPeriod  BillingPeriod      `json:"billing_period"`
+	InvoiceNumber  string             `json:"invoice_number"`
+	BillingDetails interface{}        `json:"billing_details"`
+	CollectionMode string             `json:"collection_mode"`
+	SubscriptionID string             `json:"subscription_id"`
+}
+
+type TransactionItem struct {
+	Price     Price       `json:"price"`
+	Quantity  int         `json:"quantity"`
+	Proration interface{} `json:"proration"`
+}
+
+type Price struct {
+	ID                 string        `json:"id"`
+	Name               string        `json:"name"`
+	Type               string        `json:"type"`
+	Status             string        `json:"status"`
+	Quantity           PriceQuantity `json:"quantity"`
+	TaxMode            string        `json:"tax_mode"`
+	CreatedAt          time.Time     `json:"created_at"`
+	ProductID          string        `json:"product_id"`
+	UnitPrice          UnitPrice     `json:"unit_price"`
+	UpdatedAt          time.Time     `json:"updated_at"`
+	CustomData         interface{}   `json:"custom_data"`
+	Description        string        `json:"description"`
+	TrialPeriod        interface{}   `json:"trial_period"`
+	BillingCycle       *BillingCycle `json:"billing_cycle"`
+	UnitPriceOverrides []interface{} `json:"unit_price_overrides"`
+	ImportMeta         interface{}   `json:"import_meta"`
+}
+
+type PriceQuantity struct {
+	Maximum int `json:"maximum"`
+	Minimum int `json:"minimum"`
+}
+
+type UnitPrice struct {
+	Amount       string `json:"amount"`
+	CurrencyCode string `json:"currency_code"`
+}
+
+type BillingCycle struct {
+	Interval  string `json:"interval"`
+	Frequency int    `json:"frequency"`
+}
+
+type TransactionDetails struct {
+	Totals         Totals         `json:"totals"`
+	LineItems      []LineItem     `json:"line_items"`
+	PayoutTotals   Totals         `json:"payout_totals"`
+	TaxRatesUsed   []TaxRateUsed  `json:"tax_rates_used"`
+	AdjustedTotals AdjustedTotals `json:"adjusted_totals"`
+}
+
+type Totals struct {
+	Fee             string `json:"fee"`
+	Tax             string `json:"tax"`
+	Total           string `json:"total"`
+	Credit          string `json:"credit"`
+	Balance         string `json:"balance"`
+	Discount        string `json:"discount"`
+	Earnings        string `json:"earnings"`
+	Subtotal        string `json:"subtotal"`
+	GrandTotal      string `json:"grand_total"`
+	CurrencyCode    string `json:"currency_code"`
+	CreditToBalance string `json:"credit_to_balance"`
+}
+
+type LineItem struct {
+	ID         string     `json:"id"`
+	Totals     ItemTotals `json:"totals"`
+	Product    Product    `json:"product"`
+	PriceID    string     `json:"price_id"`
+	Quantity   int        `json:"quantity"`
+	TaxRate    string     `json:"tax_rate"`
+	UnitTotals ItemTotals `json:"unit_totals"`
+}
+
+type ItemTotals struct {
+	Tax      string `json:"tax"`
+	Total    string `json:"total"`
+	Discount string `json:"discount"`
+	Subtotal string `json:"subtotal"`
+}
+
+type Product struct {
+	ID          string      `json:"id"`
+	Name        string      `json:"name"`
+	Type        string      `json:"type"`
+	Status      string      `json:"status"`
+	ImageURL    string      `json:"image_url"`
+	CreatedAt   time.Time   `json:"created_at"`
+	UpdatedAt   time.Time   `json:"updated_at"`
+	CustomData  interface{} `json:"custom_data"`
+	Description string      `json:"description"`
+	TaxCategory string      `json:"tax_category"`
+	ImportMeta  interface{} `json:"import_meta"`
+}
+
+type TaxRateUsed struct {
+	Totals  ItemTotals `json:"totals"`
+	TaxRate string     `json:"tax_rate"`
+}
+
+type AdjustedTotals struct {
+	Fee          string `json:"fee"`
+	Tax          string `json:"tax"`
+	Total        string `json:"total"`
+	Earnings     string `json:"earnings"`
+	Subtotal     string `json:"subtotal"`
+	GrandTotal   string `json:"grand_total"`
+	CurrencyCode string `json:"currency_code"`
+}
+
+type Checkout struct {
+	URL string `json:"url"`
+}
+
+type Payment struct {
+	Amount                string        `json:"amount"`
+	Status                string        `json:"status"`
+	CreatedAt             time.Time     `json:"created_at"`
+	ErrorCode             *string       `json:"error_code"`
+	CapturedAt            *time.Time    `json:"captured_at"`
+	MethodDetails         MethodDetails `json:"method_details"`
+	PaymentMethodID       string        `json:"payment_method_id"`
+	PaymentAttemptID      string        `json:"payment_attempt_id"`
+	StoredPaymentMethodID string        `json:"stored_payment_method_id"`
+}
+
+type MethodDetails struct {
+	Card *CardDetails `json:"card,omitempty"`
+	Type string       `json:"type"`
+}
+
+type CardDetails struct {
+	Type           string `json:"type"`
+	Last4          string `json:"last4"`
+	ExpiryYear     int    `json:"expiry_year"`
+	ExpiryMonth    int    `json:"expiry_month"`
+	CardholderName string `json:"cardholder_name"`
+}
+
+type BillingPeriod struct {
+	EndsAt   time.Time `json:"ends_at"`
+	StartsAt time.Time `json:"starts_at"`
+}
